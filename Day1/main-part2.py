bc326d536f1c314d97d28704a50af118df306100
4AHITS Braun Marco <old-account.marco.braun@stud.it.htl-hl.ac.at>
2025-12-02 11:56:12 +0100

    chore: refine `start` function by simplifying parameters, improving zero-crossing calculation, and adjusting dial logic

--- Day1/main-part2.py
+++ Day1/main-part2.py
@@ -1,37 +1,21 @@
 import math
 
 
-def start(is_part_two: bool = False, dial_starting_point_user: int = 50):
-    zero_count = 0
+def start(dial_starting_point_user: int = 50):
     dial_starting_point = int(dial_starting_point_user)
+    zero_count = 0
 
     content = load_file()
-    print("Starting point: " + str(dial_starting_point))
-    print("--------------------")
     for line in content.splitlines():
-        number = int(line[1:])
-        print("Rotate " + line + " beginning from " + str(dial_starting_point) + "")
+        distance = int(line[1:])
+        start_postion = dial_starting_point
 
         if line.startswith("R"):
-            dial_starting_point += number
-            zero_jumped = (dial_starting_point // 100)
-            end_position = dial_starting_point % 100
+            zero_count += math.floor(start_postion + distance / 100)
+            dial_starting_point = (start_postion + distance) % 100
         elif line.startswith("L"):
-            dial_starting_point -= number
-            zero_jumped = (dial_starting_point // 100) - math.ceil((dial_starting_point + number) / 100)
-            end_position = dial_starting_point % 100
-
-        print("Zero Jumped: " + str(zero_jumped))
-        print("New position: " + str(end_position))
-
-        if end_position == 0:
-            print("0 reached, add it to count")
-            zero_count += 1
-        if is_part_two:
-            zero_count += zero_jumped
-        print("New Zero Count: " + str(zero_count))
-        print("\n")
-    print("--------------------")
+            zero_count += math.ceil((start_postion / 100)) - math.ceil((start_postion - distance) / 100)
+            dial_starting_point = (start_postion - distance) % 100
     print("Zero Count: " + str(zero_count))
 
 
@@ -41,4 +25,4 @@ def load_file():
 
 
 if __name__ == '__main__':
-    start(True, 50)
+    start(50)
