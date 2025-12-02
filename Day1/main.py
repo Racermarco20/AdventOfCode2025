98e790d7cfc4c5d0bc3587662780189947441f0e
4AHITS Braun Marco <old-account.marco.braun@stud.it.htl-hl.ac.at>
2025-12-02 10:59:28 +0100

    chore: further refine zero-crossing logic in `start` function, adjust dial calculations, and update IntelliJ IDEA workspace configuration

--- Day1/main.py
+++ Day1/main.py
@@ -1,7 +1,6 @@
 def start(is_part_two: bool = False, dial_starting_point_user: int = 50):
     zero_count = 0
     dial_starting_point = int(dial_starting_point_user)
-    ignore_zero = False
 
     content = load_file()
     print("Starting point: " + str(dial_starting_point))
@@ -17,26 +16,22 @@ def start(is_part_two: bool = False, dial_starting_point_user: int = 50):
         elif line.startswith("L"):
             dial_starting_point -= number
         end_position = dial_starting_point % 100
-        print("End position: " + str(end_position))
         zero_jumped = (dial_starting_point // 100)
 
         if zero_jumped < 1:
             zero_jumped = zero_jumped * (-1)
-        print("Dial starting point: " + str(dial_starting_point_mod) + "")
-        if dial_starting_point_mod == 0:
+
+        if (dial_starting_point_mod == 0 or end_position == 0) and zero_jumped > 0:
             zero_jumped -= 1
             if zero_jumped < 0: zero_jumped = 0
 
         print("Zero Jumped: " + str(zero_jumped))
-
-        if zero_jumped == 1 and end_position == 0:
-            ignore_zero = True
         print("New position: " + str(end_position))
 
         if end_position == 0:
             print("0 reached, add it to count")
             zero_count += 1
-        if is_part_two and not ignore_zero:
+        if is_part_two:
             zero_count += zero_jumped
         dial_starting_point = end_position
         print("New Zero Count: " + str(zero_count))
