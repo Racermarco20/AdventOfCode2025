1276bb3bf9f3dc5af891f5a319bebf49acbf324a
4AHITS Braun Marco <old-account.marco.braun@stud.it.htl-hl.ac.at>
2025-12-02 14:43:39 +0100

    chore: enhance zero-crossing logic in `start` function, add math import, and improve debug prints

--- Day1/main-part2.py
+++ Day1/main-part2.py
@@ -1,3 +1,6 @@
+import math
+
+
 def start(dial_starting_point_user: int = 50):
     zero_count = 0
     dial_starting_point = int(dial_starting_point_user)
@@ -8,22 +11,27 @@ def start(dial_starting_point_user: int = 50):
     for line in content.splitlines():
         number = int(line[1:])
         print("Rotate " + line + " beginning from " + str(dial_starting_point) + "")
-
+        zero_count_dummy = 0
         if line.startswith("R"):
-            dial_starting_point += number
+            current_position = dial_starting_point + number
+            end_position = (current_position % 100)
+            zero_count_dummy += (current_position // 100)
         elif line.startswith("L"):
-            dial_starting_point -= number
-        end_position = dial_starting_point % 100  # -> 0
-        zero_jumped = (dial_starting_point // 100)
+            current_position = dial_starting_point - number
+            end_position = (current_position % 100)
+            if current_position == 0 or end_position == 0:
+                zero_count_dummy += 1
+            zero_count_dummy += math.ceil((current_position / -100))
+            if dial_starting_point == 0:
+                zero_count_dummy -= 1
 
-        if zero_jumped < 1:
-            zero_jumped = zero_jumped * (-1)
+            print("Zero Count dummy: " + str(zero_count_dummy) + "")
 
-        print("Zero Jumped: " + str(zero_jumped))
-        print("New position: " + str(end_position))
+        print("Zero Count in this round: " + str(zero_count_dummy) + "")
+        zero_count += zero_count_dummy
 
-        zero_count += zero_jumped
         dial_starting_point = end_position
+        print("New position: " + str(dial_starting_point))
         print("New Zero Count: " + str(zero_count))
         print("\n")
 
